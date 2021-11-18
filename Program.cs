1fa102380f5ea5a04549042611631e0b2e149140
Srg90 <usikov90@mail.ru>
2021-11-18 23:03:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите целое положительное число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int sqr = 0;
+            for (int i = 1; i <= (2 * n - 1); i += 2)
+            {
+                sqr += i;
+                Console.WriteLine("Квадрат равен {0}", sqr);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Квадрат числа {0} равен {1}", n, sqr);
+            Console.ReadKey();
+
+        }
+    }
+}
